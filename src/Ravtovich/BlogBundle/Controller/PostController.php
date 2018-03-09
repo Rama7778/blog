df80a39c5cf8c86ff67dd560c8c08979ae8c5bd4
yaurau <rama7777@bk.ru>
2018-03-09 20:00:57 +0300

    Code update

--- src/Ravtovich/BlogBundle/Controller/PostController.php
+++ src/Ravtovich/BlogBundle/Controller/PostController.php
@@ -12,14 +12,18 @@ class PostController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $blog = $em->getRepository('RavtovichBlogBundle:Post')->find($id);
-
         if (!$blog) {
             throw $this->createNotFoundException('Unable to find Blog post.');
         }
 
+        $comments = $em->getRepository('RavtovichBlogBundle:Comment')
+            ->getCommentsForBlog($blog->getId());
+
         return $this->render('RavtovichBlogBundle:Post:show.html.twig', array(
             'blog'      => $blog,
+            'comments'  => $comments
         ));
+
     }
 
 }
--- src/Ravtovich/BlogBundle/DataFixtures/ORM/BlogFixtures.php
+++ src/Ravtovich/BlogBundle/DataFixtures/ORM/BlogFixtures.php
@@ -1,11 +1,14 @@
 <?php
 namespace Ravtovich\BlogBundle\DataFixtures\ORM;
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
+
 use Doctrine\Common\Persistence\ObjectManager;
 use Ravtovich\BlogBundle\Entity\Post;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
-class BlogFixtures implements FixtureInterface
+
+class BlogFixtures extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -60,6 +63,18 @@ class BlogFixtures implements FixtureInterface
         $manager->persist($blog5);
 
         $manager->flush();
+
+        $this->addReference('blog-1', $blog1);
+        $this->addReference('blog-2', $blog2);
+        $this->addReference('blog-3', $blog3);
+        $this->addReference('blog-4', $blog4);
+        $this->addReference('blog-5', $blog5);
     }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+
 
 }
