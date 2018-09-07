066160efc27c411af19397eb2f51248dc8e1f792
yaurau <rama7777@bk.ru>
2018-09-07 18:38:00 +0300

    Moving on to Symfony 4 07/09_2

---
+++ src/Twig/RavtovichBlogExtension.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class RavtovichBlogExtension extends  AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('created_ago', [$this, 'createdAgo']),
+        ];
+    }
+
+    public function createdAgo(\DateTime $dateTime)
+    {
+        $delta = time() - $dateTime->getTimestamp();
+        if ($delta < 0)
+            throw new \InvalidArgumentException("createdAgo is unable to handle dates in the future");
+
+        $duration = "";
+        if ($delta < 60)
+        {
+            // Seconds
+            $time = $delta;
+            $duration = $time . " second" . (($time > 1) ? "s" : "") . " ago";
+        }
+        else if ($delta <= 3600)
+        {
+            // Mins
+            $time = floor($delta / 60);
+            $duration = $time . " minute" . (($time > 1) ? "s" : "") . " ago";
+        }
+        else if ($delta <= 86400)
+        {
+            // Hours
+            $time = floor($delta / 3600);
+            $duration = $time . " hour" . (($time > 1) ? "s" : "") . " ago";
+        }
+        else
+        {
+            // Days
+            $time = floor($delta / 86400);
+            $duration = $time . " day" . (($time > 1) ? "s" : "") . " ago";
+        }
+
+        return $duration;
+    }
+
+    public function getName()
+    {
+        return 'ravtovich_blog_extension';
+    }
+}
