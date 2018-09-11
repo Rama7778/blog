32722b8b0c6fe3ec44b42b56daedfe29798c64bd
yaurau <rama7777@bk.ru>
2018-09-12 01:00:09 +0300

    Moving on to Symfony 4 12/09

--- src/DataFixtures/ORM/UserFixtures.php
+++ src/DataFixtures/ORM/UserFixtures.php
@@ -5,14 +5,25 @@ namespace App\DataFixtures\ORM;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class UserFixtures extends Fixture
 {
+    private $container;
+
+    public function __construct( ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
     public function load(ObjectManager $manager)
     {
+
+        $encoder = $this->container->get('security.password_encoder');
         $user = new User();
         $user->setUsername('123456');
-        $user->setPassword('123456');
+        $password = $encoder->encodePassword($user, '123456');
+        $user->setPassword($password);
         $user->setEmail('info@utilvideo.com');
         $user->setIsactive(1);
         $manager->persist($user);
